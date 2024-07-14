4d2d6784a6c5f2f85b9cd4620d87e258532ccc92
Todd, Jacob <jtodd@verisk.com>
2024-07-14 12:02:01 -0500

    Adding in functions and interfaces to handle the shortener

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 import {URLs} from './mockData'
+import {IURL} from './Interface'
+
+const shortURL = ""
+
+def createShortURL(longURL:string):IURL {
+    return {
+
+    }
+}
 
 console.log(URLs.youTube)
