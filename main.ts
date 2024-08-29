90384e961c8983c36cd9621263e817dd6ad211e3
Todd, Jacob <jtodd@verisk.com>
2024-08-29 11:46:58 -0500

    Fixed the RegEx that was causing an issue.

--- main.ts
+++ main.ts
@@ -3,33 +3,9 @@ import { URLs } from './mockData'
 import { DataStoreFactory } from './class/dataStoreFactoryClass'
 import { DataStoreType } from './dataStore'
 
-//Way 1
-// export function requestURL(URL: string): string {
-//     const urlObject = URLs.find(url => url.shortURL === URL);
-//     if (urlObject) {
-//         console.log(urlObject.longURL)
-//         return urlObject.longURL;
-//     } else {
-//         console.log("URL not found")
-//         return "URL not found";
-//     }
-// }
-//
-// const url = requestURL('youtube');
-
-//Way 2
-// async function main() {
-//     const localDataStore = await DataStoreFactory.MakeDataStore(DataStoreType.LOCAL)
-//     const shortUrl = localDataStore.makeShortURL('https://www.amazon.com/')
-//     const longUrl = localDataStore.getLongURL(shortUrl);
-//     console.log(longUrl);
-//     console.log(shortUrl);
-// }
-
-// Way 3 This one will take what ever URL you give it and allow you to get the short URL or the long URL
 async function main(URL: string) {
     const localDataStore = await DataStoreFactory.MakeDataStore(DataStoreType.LOCAL)
-    const regex = /(https?:\/\/)? (www\.)? (\.com)? /;
+    const regex = /^[^/.]+$/;
     const isShortURL = regex.test(URL);
     // const isExistsShortURL = URLs.some(url => url.shortURL === URL);
     console.log(isShortURL);
@@ -44,7 +20,7 @@ async function main(URL: string) {
 }
 
 
-main("www.ryan.com");
+main("youtube");
 
 
 //TODO: Show that I have so far
